c0e1da2a0d8033f1c19c2ceb8fd556ceda977a57
naresh.ko <naresh.ko@imimobile.com>
2020-02-26 14:48:11 +0530

    WK web view added and loading url with indicator

--- sample/WebViewController.swift
+++ sample/WebViewController.swift
@@ -7,26 +7,61 @@
 //
 
 import UIKit
+import WebKit
+class WebViewController: UIViewController, WKNavigationDelegate, WKUIDelegate  {
 
-class WebViewController: UIViewController {
-
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet var webViewContainer: UIView!
+    var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         //unhide navigation bar
         self.navigationController?.navigationBar.isHidden = false
         
+        //Load url in webview
+//           let url = URL(string: "https://www.youtube.com")
+//           webView.load(URLRequest(url: url!))
+        
+        //delegate method for webview
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        
+        
+        activityIndicator = UIActivityIndicatorView()
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.style = UIActivityIndicatorView.Style.gray
+
+        self.view.addSubview(activityIndicator)
+        
+        webView.load(URLRequest(url: URL(string: "https://www.youtube.com")!))
+
+
+    }
+    
+    
+    //webview delegate methods
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error)
+    {
+          print(error.localizedDescription)
+        activityIndicator.stopAnimating()
+     }
+     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!)
+     {
+          //        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+          print("Strat to load")
+        activityIndicator.startAnimating()
+     }
+     
+
+    
+   func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activityIndicator.stopAnimating()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        activityIndicator.stopAnimating()
     }
-    */
 
 }
